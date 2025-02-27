c0e28d286eb1021c2d5d5ac4ffff911a00210384
Rafinkhan1 <mdrafinkhan827@gmail.com>
2025-02-28 03:38:17 +0600

    js functionality added

--- triangle.js
+++ triangle.js
@@ -1,17 +1,56 @@
-function triangleCalculation(){
 
-    const traingleBase = document.getElementById('triangle-base');
+
+function areaCalculation(){
+    const base = commonCalculation('triangle-base');
+    const height = commonCalculation('triangle-height');
+    const area = commonAreaCalculation(base,height);
+    showOnsite('triangle-area',area*0.5);   
+}
+function ellipseArea(){
+    const a = commonCalculation('ellipse-base');
+    const b = commonCalculation('ellipse-height');
+    const ellipseArea = commonAreaCalculation(a,b);
+    showOnsite('ellipse-area',ellipseArea*3.1416);
+}
+function rectangleArea(){
+    const a = commonCalculation('rectangle-base');
+    const b = commonCalculation('rectangle-height');
+    const area = commonAreaCalculation(a,b);
+    showOnsite('rectangle-area',area);
+}
+function parallelogramArea(){
+    const a = commonCalculation('parallelogram-base');
+    const b = commonCalculation('parallelogram-height');
+    const area = commonAreaCalculation(a,b);
+    showOnsite('parallelogram-area',area);
+}
+function pentagonArea(){
+    const a = commonCalculation('pentagon-base');
+    const b = commonCalculation('pentagon-height');
+    const area = commonAreaCalculation(a,b);
+    showOnsite('pentagon-area',area*0.5);
+}
+function rhombusArea(){
+    const a = commonCalculation('rhombus-base');
+    const b = commonCalculation('rhombus-height');
+    const area = commonAreaCalculation(a,b);
+    showOnsite('rhombus-area',area*0.5);
+}
+
+
+function commonCalculation(inputFieldId){
+
+    const traingleBase = document.getElementById(inputFieldId);
     const triangleBaseInput = traingleBase.value;
-    const base = parseFloat(triangleBaseInput);
-    //console.log(base);
+    const inputValue = parseFloat(triangleBaseInput);
+    return inputValue;
+}
 
-    const traingleHeight = document.getElementById('triangle-height');
-    const triangleHeightInput = traingleHeight.value;
-    const height = parseFloat(triangleHeightInput);
-    //console.log(height);
-
-    const result = 0.5* base*height;
-
-    const triangleAreaShow = document.getElementById('triangle-area');
-    triangleAreaShow.innerText = result;
+function commonAreaCalculation(num1, num2){
+    const areaValue = num1*num2;
+    return areaValue;
+}
+function showOnsite(takeShowPlace,area){
+    const areaShowOnSite = document.getElementById(takeShowPlace);
+    areaShowOnSite.innerText = area;
 }
